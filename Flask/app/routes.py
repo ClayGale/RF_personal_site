087ef5e77bf14a948079500c8e3d0ff7cc4a8753
ClayGale <clay.aj.gale@gmail.com>
2022-01-18 23:04:37 -0800

    Initialization
    Combining personal site files and react flask framework

---
+++ Flask/app/routes.py
@@ -0,0 +1,21 @@
+import os, json
+from flask import render_template, flash, redirect
+from app import app
+
+@app.route('/')
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html', title='home')
+
+@app.route('/projects') #route for projects showcase including json project data loading
+def projects():
+    projects = os.path.join(app.static_folder, 'data', 'projects.json')
+
+    with open(projects) as pData:
+        data = json.load(pData)
+
+    return render_template('projects.html', title='showcase', data=data)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='contact')
