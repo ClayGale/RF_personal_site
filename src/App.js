8fd931d80181b5d27bdb2119e6752e1c4ab9930b
ClayGale <clay.aj.gale@gmail.com>
2022-03-02 22:29:11 -0800

    Async backend requests working!
    Major refactoring of the contactBackend code

--- src/App.js
+++ src/App.js
@@ -1,73 +1,30 @@
-import React, { useState, useEffect, useRef, useReducer} from 'react';
+import React, { useState, useEffect} from 'react';
 import './main.css';
 import Frame from './Components/Frame';
 
-const ACTIONS = {
-    SET_CONTENT: 'set-content',
-    GET_CONTENT: 'get-content'
-}
-
-function reducer(content, action) {
-    switch (action.type) {
-        case action.SET_CONTENT:
-            console.log('setting ' + action);
-            return content[action.payload.key] = action.payload.htmlPack
-        default:
-            return content
-    }
-}
 
 function App() {
-    const [view, setView] = useState('');
-    //const [loading, setLoading] = useState(true);
-    const [content, dispatch] = useReducer(reducer, []);
-    //const mainContent = useRef(null);
-    //const navBar = useRef(null);
-    setView(window.location.pathname)
-    //if (view === '/') { console.log('correcting view value'); setView('index') }
-    //useEffect(() => {
-        
+    const [view, setView] = useState(window.location.pathname);
 
-    //    console.log('logging content ' + content[view]);
+    //setView(window.location.pathname)
+    useEffect(() => {
+
+        setView(window.location.pathname);
+        console.log('view change ' + view);
         //setLoading(false);
-    //}, [])
-
-    const contactBackend = (target) => {
-        fetch('http://localhost:5000/' + target, {
-            'methods': 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .then(responseData => {
-                console.log(responseData.htmlPack);
-                return responseData.htmlPack;
-            })
-            .catch(error => console.log(error))
-    };
+    }, [window.location.pathname]);
 
 
-    console.log('hello');
-    let one = contactBackend(view);
-    console.log(one);
-    let two = contactBackend('nav');
-    console.log(two);
-    dispatch({
-        type: ACTIONS.SET_CONTENT,
-        payload: { key: view, htmlPack: one }
-    });
+
     console.log('logging view ' + view);
-    console.log('logging content array ' + content);
-    //navBar = useRef(contactBackend('nav'));
-    console.log('logging content ' + content[view]);
-    console.log('logging nav ' + content.nav);
 
     //if (loading) { return <div className="App"> loading </div>}
     return (
         <div className="App container m-4">
-            <Frame viewF={content.nav} />
-            <Frame viewF={content[view]} />
+            <Frame viewF={'/nav'} />
+            <div id='content'>
+                <Frame viewF={view} />
+            </div>
         </div>
     );
 
--- src/Components/Frame.js
+++ src/Components/Frame.js
@@ -1,11 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
 function createMarkup(markup) {
     return { __html: markup };
 }
 
 const Frame = (props) => {
+    const [content, setContent] = useState('');
+
+    let target = props.viewF;
+    if (target === '/') { target = '/index' };
+    useEffect(() => {
+        const contactBackend = async () => {
+            try {
+                const response = await fetch('http://localhost:5000' + target, {
+                    'methods': 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                const data = await response.json();
+                console.log(data);
+                setContent(data.htmlPack);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        contactBackend();
+    }, []);
+
 
     return (
-        <div className="m-2" dangerouslySetInnerHTML={createMarkup(props.viewF)}>
+        <div className="m-2" dangerouslySetInnerHTML={createMarkup(content)}>
 
         </div>
     )
