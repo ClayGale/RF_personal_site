8be14a41e927c177349a2b71045dff028c9e9905
ClayGale <clay.aj.gale@gmail.com>
2022-04-12 10:20:34 -0700

    Horizontal scrolling functional
    Needs some tweaking for smoothness but is good for now

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -1,27 +1,26 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useRef } from 'react';
 import Showcase from './Showcase';
 
 const Preview = (props) => {
-    const [showcase, setShowcase] = useState('');
-
+    const [showcase, setShowcase] = useState(''); //state for the currently active showcase component
+    const scrollingBox = useRef();
     function handleShowcaseRequest(ID) {
         console.log(ID);
         setShowcase(ID);
     };
 
-    //let prevScroll = 0;
-    //let translateScroll = 0;
+    /* horizontalScroll is pointed to by the onwheel events of both the main scrolling component
+    and the individual containers so horizontal scrolling is achieved no matter where the mouse 
+    is pointing within the div*/
     function horizontalScroll(event) {
-        console.log(event.deltaY + ' ' + event.deltaX);
-        /*event.target.scrollLeft = event.target.scrollLeft + event.deltaY * 2;*/
-        event.target.scroll({
-            left: event.target.scrollLeft + event.deltaY * 5,
+        scrollingBox.current.scroll({
+            left: event.target.scrollLeft + event.deltaY * 3.5,
             behavior: 'smooth'
         });
     };
 
     console.log(props);
-    const previewItems = useMemo(() => {
+    const previewItems = useMemo(() => { //memoizing the built list so it only rebuilds on a page change
 
         switch (props.type) {
 
@@ -30,7 +29,7 @@ const Preview = (props) => {
                 console.log('rebuild');
                 props.data.projects.forEach((project) => {
                     projectPrevs.push(
-                        <div key={project.ID} value={project.ID} className='project' onClick={() => handleShowcaseRequest(project.ID)}>
+                        <div key={project.ID} value={project.ID} className='project' onWheel={horizontalScroll} onClick={() => handleShowcaseRequest(project.ID)}>
                             <h1>{project.title}</h1>
                             <a href={project.link}> {project.linkdesc} </a>
                             <p> {project.shortdesc} </p>
@@ -43,7 +42,7 @@ const Preview = (props) => {
                 console.log('rebuild');
                 props.data.classes.forEach((schoolClass) => {
                     classPrevs.push(
-                        <div className='schoolContainer'>
+                        <div className='schoolContainer' onWheel={horizontalScroll}>
                             <div key={schoolClass.ID} value={schoolClass.ID} className='schoolClass'>
                                 <h1>{schoolClass.ID}</h1>
                                 <h2>{schoolClass.title}</h2>
@@ -59,12 +58,12 @@ const Preview = (props) => {
                 break;
         }
     }, [props.type]);
-    //console.log(prevs);
+    
     return (
         <section >
             <h4> {props.type} </h4>
             <Showcase showcase={showcase} setShowcase={setShowcase} />
-            <div id={props.type} className="snapping scrolling" onWheel={horizontalScroll}>{previewItems}</div>
+            <div ref={scrollingBox} id={props.type} className="snapping scrolling" onWheel={horizontalScroll}>{previewItems}</div>
         </section>
     );
 }
